311ea38cbd83e256fb3d588dd93b4312423077b5
VictoriyaK19 <kalinovavictoriya@gmail.com>
2023-06-07 15:20:30 +0300

    conditional rendering

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image: './assets/images/socks_green.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 0,
+            
         }
     }
 })
